c0300a10409b602eb58e3b6425e229a787667332
AllMightyDude <32651139+AllMightyDude@users.noreply.github.com>
2019-08-23 15:18:09 +0100

    Update models.py

--- accident/models/models.py
+++ accident/models/models.py
@@ -1,36 +1,28 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+import datetime
+from odoo import models, fields, api , _
 
 class regle(models.Model):
     _name = 'reg.reg'
     _rec_name = 'nom'
 
-    nom = fields.Char(
-        string="Règle",
-        required=False, )
-    
-    icon = fields.Binary(
-        string="Icone",  )
-    
-    desc = fields.Char(
-        string="Description"
-        , required=False, )
+    nom = fields.Char(string="Règle", required=False, )
+    icon = fields.Binary(string="Icone",  )
+    desc = fields.Char(string="Description", required=False, )
 
 class gravite(models.Model):
     _name = 'gra.gra'
     _rec_name = 'grav'
 
-    grav = fields.Char(
-        string="Gravité",
-        required=False, )                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+    grav = fields.Char(string="Gravité", required=False, )                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
 
 
 class accident(models.Model):
     _name = 'acc.acc'
     _rec_name = 'type'
-    
-     id_soumetteur = fields.Many2one(
+
+    id_soumetteur = fields.Many2one(
         'res.users',
         string='Agent',
         default=lambda s: s.env.user,
@@ -44,37 +36,14 @@ class accident(models.Model):
         copy=False,
         default='draft',
         required=True)
-    
-    type = fields.Selection(
-        string="Type",
-        selection=[('Accident', 'Accident'),
-                   ('Incident', 'Incident'), ],
-        required=False, )
-    
-    gravite = fields.Many2one(
-        'gra.gra',
-        string="Gravité",
-        required=False, )
-    
-    regle = fields.Many2one(
-        'reg.reg',
-        string="Règle de sécurité",
-        required=False, )
-    
-    date = fields.Datetime(
-        string="Date",
-        default=lambda s: fields.Datetime.now(),
-        invisible=False,
-        readonly=True,
-        required=False, )
-    
-    descaccident = fields.Char(
-        string="Déscription",
-        required=False, )
-    
-    img = fields.Binary(
-        string="Photo",  )
-    
+    type = fields.Selection(string="Type", selection=[('Accident', 'Accident'), ('Incident', 'Incident'), ], required=False, )
+    gravite = fields.Many2one('gra.gra', string="Gravité", required=False, )
+    regle = fields.Many2one('reg.reg', string="Règle de sécurité", required=False, )
+    date = fields.Datetime(string="Date",default=lambda s: fields.Datetime.now(),invisible=False,readonly=True, required=False, )
+    descaccident = fields.Char(string="Déscription", required=False, )
+    img = fields.Binary(string="Photo",  )
+
+
     @api.model
     def create(self, vals):
         if vals:
@@ -130,4 +99,3 @@ class accident(models.Model):
     @api.multi
     def refuser_observation(self):
         return self.write({'state': 'blocked'})
-
