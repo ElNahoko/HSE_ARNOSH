22c58af4440daebe92a85d05b6331859d83e6d16
AllMightyDude <32651139+AllMightyDude@users.noreply.github.com>
2019-08-28 21:56:08 +0100

    Update models.py

--- accident/models/models.py
+++ accident/models/models.py
@@ -14,6 +14,8 @@ class regle(models.Model):
 class accident(models.Model):
     _name = 'acc.acc'
     _rec_name = 'type'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+
 
     id_soumetteur = fields.Many2one(
         'res.users',
@@ -100,4 +102,39 @@ class accident(models.Model):
     def refuser_observation(self):
         return self.write({'state': 'blocked'})
 
-
+    @api.multi
+    def action_quotation_send(self):
+        '''
+        This function opens a window to compose an email, with the edi sale template message loaded by default
+        '''
+        self.ensure_one()
+        ir_model_data = self.env['ir.model.data']
+        try:
+            template_id = ir_model_data.get_object_reference('accident', 'email_template_edi_sale')[1]
+        except ValueError:
+            template_id = False
+        try:
+            compose_form_id = ir_model_data.get_object_reference('mail', 'email_compose_message_wizard_form')[1]
+        except ValueError:
+            compose_form_id = False
+        ctx = {
+            'default_model': 'acc.acc',
+            'default_res_id': self.ids[0],
+            'default_use_template': bool(template_id),
+            'default_template_id': template_id,
+            'default_composition_mode': 'comment',
+            'mark_so_as_sent': True,
+            'custom_layout': "mail.mail_notification_paynow",
+            'proforma': self.env.context.get('proforma', False),
+            'force_email': True
+        }
+        return {
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(compose_form_id, 'form')],
+            'view_id': compose_form_id,
+            'target': 'new',
+            'context': ctx,
+        }
