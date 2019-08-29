63f5b0436917a9dc7556966aea71937c9a366b18
drakusCode <41284731+drakusCode@users.noreply.github.com>
2019-08-30 01:30:18 +0200

    module controles équipements
    Tache de controles : en cours ..

---
+++ controle_equipement/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "controle_equipement",
+
+    'summary': """
+        module de gestion et contrôles d'équipements ..""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','mail'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/Equipement_VIEW.xml',
+        'data/Equipement_sequence.xml',
+        'views/Localisation_VIEW.xml',
+        'views/categorie_VIEW.xml',
+        'views/alert_control_VIEW.xml',
+        'data/Controle_sequence.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
