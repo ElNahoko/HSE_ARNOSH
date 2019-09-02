76bd89913e98948a6cd9de13bef3a9f1995e413a
Adham baquroun <44381355+ElNahoko@users.noreply.github.com>
2019-09-02 17:41:53 +0100

    Update __manifest__.py

--- workflow/__manifest__.py
+++ workflow/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','mail'],
 
     # always loaded
     'data': [
@@ -29,6 +29,7 @@
         'security/ir.model.access.csv',
         'views/workflow_kanban.xml',
         'views/workflow_form.xml',
+        'views/zone_fom.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
