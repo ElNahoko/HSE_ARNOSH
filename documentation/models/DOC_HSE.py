6d594e4c16448e56ec296d5f5af501074dccfb1f
AllMightyDude <32651139+AllMightyDude@users.noreply.github.com>
2019-09-02 09:41:35 +0100

    Update DOC_HSE.py

--- documentation/models/DOC_HSE.py
+++ documentation/models/DOC_HSE.py
@@ -5,7 +5,7 @@ from odoo import models, fields, api
 
 class DOCHSE(models.Model):
     _name = 'document'
-    _rec_name = 'Document_binaire'
+    _rec_name = 'categorie'
 
     categorie = fields.Many2one(
         'document.categorie',
@@ -19,11 +19,21 @@ class DOCHSE(models.Model):
         string="Filename",
         required=False, )
 
+    @api.multi
+    def print_doc(self):
+        return self.env.ref('documentation.action_report_doc').report_action(self)
+
 class DOCUMENTCATEGORIE(models.Model):
     _name = 'document.categorie'
     _rec_name = 'categorie'
 
     categorie = fields.Char(
         string="catégorie",
+        required=False, )
+    date = fields.Datetime(
+        string="Date",
+        default=lambda s: fields.Datetime.now(),
+        invisible=False,
+        readonly=True,
         required=False, )
 
