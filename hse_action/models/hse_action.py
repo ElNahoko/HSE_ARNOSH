03e5d268be17b6accd6cfe840b2a1ebd481f82fd
Adham baquroun <44381355+ElNahoko@users.noreply.github.com>
2019-09-05 13:51:53 +0100

    Update hse_action.py

--- hse_action/models/hse_action.py
+++ hse_action/models/hse_action.py
@@ -7,6 +7,7 @@ class Action(models.Model):
     _name = "action"
     _rec_name = "reference_action"
 
+    name = fields.Char('Subject', required=True)
     # N° de référence de l'action pour permettre un suivis
     reference_action = fields.Char(
         string="Reference Action",
@@ -16,7 +17,7 @@ class Action(models.Model):
     # Indiquer d’où vient l'action.
     source = fields.Char(
         string="Type Risque",
-        required=False, readonly=True,)
+        required=False, readonly=True, )
 
     # Description de l'observation
     description_observation = fields.Text(
@@ -25,16 +26,14 @@ class Action(models.Model):
 
     # La personne qui a fait l'observation
     originateur = fields.Char(
-        string='Agent',
-        readonly=True, )
+        string='Agent', )
 
     #  Catégories SOR
     categorie = fields.Char(
         string="Categorie",
-        required=False,
-        readonly=True,)
+        required=False, )
 
-    #Action prise ou à prendre pour corriger l'anomalie
+    # Action prise ou à prendre pour corriger l'anomalie
     action_correctif = fields.Text(
         string="Action Correctif",
         required=False, )
@@ -47,10 +46,9 @@ class Action(models.Model):
     # Closed = Cloturée; Open = Ouvert
     etat = fields.Selection(
         string="Etat",
-        selection=[('Cloturee', 'Closed'),
-                   ('Ouvert', 'Open'), ],
-        required=False,
-        readonly=True, )
+        selection=[('Cloturee', 'Action terminée'),
+                   ('Ouvert', 'Action en cours'), ],
+        required=False, )
 
     # Responsable du suivis de l'action
     responsable = fields.Char(
@@ -60,9 +58,8 @@ class Action(models.Model):
     # L'entreprise chez laquelle l'observation a été faite
 
     entreprise = fields.Char(
-        string = "Entreprise",
-        required=False,
-        readonly=True)
+        string="Entreprise",
+        required=False, )
 
     # Si l'observation est un non respect des consignes de sécurité mettre "Finding".
     # Si l'observation est positive mettre "Positive Observation"
@@ -84,7 +81,20 @@ class Action(models.Model):
         required=False,
         default=lambda s: fields.Datetime.now(),
         readonly=True)
-    
+
+    # Cause Racine Description de la cause racine du problème.
+    Cause_Racine = fields.Char(
+        string="Cause Racine ",
+        required=False,
+        readonly=False)
+
+    type_action = fields.Selection([
+        ('immediate', 'Action immédiate'),
+        ('Corrective', 'Action Corrective'),
+        ('Preventive', 'Action Préventive'),
+        ('amelioration', 'Possibilité Amélioration')
+    ], 'Type Action', required=True)
+
     @api.model
     def create(self, vals):
         if vals.get('source') == 'SOR':
@@ -96,3 +106,20 @@ class Action(models.Model):
         elif vals.get('source') == 'Incident':
             vals['reference_action'] = self.env['ir.sequence'].next_by_code('action.inci') or _('New')
             return super(Action, self).create(vals)
+        else:
+            vals['reference_action'] = self.env['ir.sequence'].next_by_code('action.plan') or _('New')
+            return super(Action, self).create(vals)
+# @api.model
+    # def process_reminder_queue(self, reminder_days=1):
+    #   """Notify user when we are 10 days close to a deadline."""
+    ## cur_date = datetime.now().date() + datetime.timedelta(minutes=reminder_days)
+    #  actions = self.search([
+    #     ("etat", "=", 'Ouvert')
+    # ])
+    # if actions:
+    #   template = self.env.ref(
+    #      'hse_action.action_email_template_reminder_action')
+    # for action in actions:
+    #    template.send_mail(action.id)
+    # return True
+    # return False
